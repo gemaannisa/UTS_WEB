67a44cad5ab2e63d669685f5f2113ff21bdeb137
gemaannisa <gemaannisa05@gmail.com>
2020-10-16 16:57:08 +0700

    praktikum/16-10-2020

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -8,5 +8,11 @@ class Home extends BaseController
 	}
 
 	//--------------------------------------------------------------------
-
+	public function show(){
+		$data['nama']= 'Gema Annisa Hermastuti';
+		$data['jurusan']= 'Ilmu Komputer';
+		echo view('mahasiswa/header');
+		echo view('mahasiswa/index', $data);
+		echo view('mahasiswa/footer');
+	}
 }
---
+++ app/Views/mahasiswa/index.php
@@ -0,0 +1,9 @@
+
+<h1>Tabel Mahasiswa</h1>
+<table>
+<tr>
+<td><?= $nama ?></td>
+</tr>
+<tr>
+<td><?= $jurusan ?></td>
+</tr>
